5c7a28ff8545c9925071e6aea9ab76b6fbb92b11
ozumin <mizuo.n@gmail.com>
2020-04-21 15:02:21 +0900

    fix minor issue

--- src/background.js
+++ src/background.js
@@ -1,8 +1,10 @@
 chrome.browserAction.onClicked.addListener(function (tab) {
-  if (tab.url.indexOf("https://github.com/") != -1){
-    chrome.tabs.executeScript(tab.id, {
-      "file": "content.js"
-    }, function (){
-    })
+  if (tab && tab.url) {
+    if (tab.url.match( /https:\/\/github.com.*?\/wiki.*?/ )){
+      chrome.tabs.executeScript(tab.id, {
+        "file": "content.js"
+      }, function (){
+      })
+    }
   }
 })
